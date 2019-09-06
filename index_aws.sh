191d2c75a13b9d608b0a746b36cc514c96db354c
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-06 14:17:40 +1000

    bug fix

--- index_aws.sh
+++ index_aws.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-datacube product add ../config/products/ls5_usgs_sr_scene.yaml
-datacube product add ../config/products/ls8_usgs_sr_scene.yaml
-datacube product add ../config/products/ls_usgs_fc_scene.yaml
+datacube product add ../deafrica_config/products/ls5_usgs_sr_scene.yaml
+datacube product add ../deafrica_config/products/ls8_usgs_sr_scene.yaml
+datacube product add ../deafrica_config/products/ls_usgs_fc_scene.yaml
 
-python3 /opt/cube-in-a-box/scripts/ls_public_bucket.py test-results-deafrica-st\
+python3 ../cube-in-a-box/scripts/ls_public_bucket.py test-results-deafrica-st\
 aging-west -p test  --suffix=".xml"  --start_date 1980-01-01 --end_date 2020-01\
 -01
 
